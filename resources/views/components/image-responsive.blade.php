46fabc5f15599810a27e982273e85de332dc17a2
Evertjan Garretsen <evertjan@mlbrgn.nl>
2025-05-24 01:56:51 +0200

    responsive-image refactor now checks if conversion exists

--- resources/views/components/image-responsive.blade.php
+++ resources/views/components/image-responsive.blade.php
@@ -1,1 +1,14 @@
-{{ $medium->img()->lazy()->attributes(['class' => 'w-100 h-100 object-cover']) }}
+<img
+    @if($hasGeneratedConversion)
+        src="{{ $medium->getUrl($conversion) }}"
+        srcset="{{ $medium->getSrcset($conversion) }}"
+        sizes="{{ $sizes }}"
+    @else
+        src="{{ $medium->getUrl() }}"
+    @endif
+    {{ $attributes->merge(['class' => '']) }}
+    @if($lazy ?? true)
+        loading="lazy"
+    @endif
+    alt="{!! $alt !!}"
+/>
--- src/View/Components/ImageResponsive.php
+++ src/View/Components/ImageResponsive.php
@@ -6,14 +6,36 @@ use Illuminate\View\Component;
 use Illuminate\View\View;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
+/**
+ * Represents a responsive image component.
+ *
+ * This component is used for rendering an image with responsive attributes.
+ *
+ * @param  Media  $medium  The media object representing the image.
+ * @param  string  $conversion  The conversion format for the image. Defaults to an empty string.
+ * @param  string  $sizes  The sizes attribute for responsive images. Defaults to '100vw'.
+ * @param  array  $attributes  Additional HTML attributes for the image. Defaults to an empty array.
+ * @param  bool  $lazy  Indicates whether lazy loading is enabled for the image. Defaults to true.
+ * @return View The view for rendering the responsive image component.
+ */
 class ImageResponsive extends Component
 {
+    public bool $hasGeneratedConversion = false;
+
     public function __construct(
-        public ?Media $medium = null,
+        public Media $medium,
+        public string $conversion = '',
+        public string $sizes = '100vw',
+        public $attributes = [],
+        public bool $lazy = true,
+        public string $alt = ''
     ) {}
 
     public function render(): View
     {
+
+        $this->hasGeneratedConversion = $this->medium->hasGeneratedConversion('16x9');
+
         return view('media-library-extensions::components.image-responsive');
     }
 }
