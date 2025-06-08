b2b87befb67d81aeb686f9ae12230dd8a14cce1a
Evertjan Garretsen <evertjan@mlbrgn.nl>
2025-06-08 13:43:23 +0200

    more uniform styling of modal and carousel controls

--- resources/views/components/bootstrap-5/media-modal.blade.php
+++ resources/views/components/bootstrap-5/media-modal.blade.php
@@ -15,10 +15,21 @@
                 <div class="media-modal-body modal-body p-0">
                     <button
                         type="button"
-                        class="media-modal-close-button btn-close"
+                        class="media-modal-close-button"
                         data-bs-dismiss="modal"
                         aria-label="Sluit"
-                        title="{{ __('media-library-extensions::messages.close') }}"></button>
+                        title="{{ __('media-library-extensions::messages.close') }}">
+                        <x-mle-partial-icon
+                            name="{{ config('media-library-extensions.icons.close') }}"
+                            title="{{ __('media-library-extensions::messages.close') }}"
+                        />
+                    </button>
+{{--                    <button--}}
+{{--                        type="button"--}}
+{{--                        class="media-modal-close-button btn-close"--}}
+{{--                        data-bs-dismiss="modal"--}}
+{{--                        aria-label="Sluit"--}}
+{{--                        title="{{ __('media-library-extensions::messages.close') }}"></button>--}}
 
                     {{-- important set click to open in modal to false otherwise endless inclusion --}}
                     <x-mle-media-carousel
