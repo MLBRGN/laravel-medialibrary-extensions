8bbc88c1a958ec37c8ffbe9ad026f837b8361212
Evertjan Garretsen <evertjan@mlbrgn.nl>
2025-06-18 20:39:44 +0200

    small fixes

--- src/View/Components/Partials/SetAsFirstForm.php
+++ src/View/Components/Partials/SetAsFirstForm.php
@@ -4,18 +4,25 @@
 
 namespace Mlbrgn\MediaLibraryExtensions\View\Components\Partials;
 
+use Illuminate\Support\Collection;
 use Illuminate\View\View;
 use Mlbrgn\MediaLibraryExtensions\View\Components\BaseComponent;
+use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class DestroyForm extends BaseComponent
+class SetAsFirstForm extends BaseComponent
 {
     public function __construct(
-
+        public Collection $media,
         public Media $medium,
         public string $id,
         public ?string $frontendTheme,
         public ?bool $useXhr = null,
+        public string $mediaCollection = '',
+//        public string $youtubeCollection = '',
+//        public string $documentCollection = '',
+        public bool $setAsFirstEnabled = false,
+        public ?HasMedia $model,
     ) {
         parent::__construct($id, $frontendTheme);
     }
@@ -23,7 +30,6 @@ class DestroyForm extends BaseComponent
     public function render(): View
     {
         $this->useXhr = !is_null($this->useXhr) ? $this->useXhr : config('media-library-extensions.use_xhr');
-
-        return $this->getPartialView('destroy-form', $this->theme);
+        return $this->getPartialView('set-as-first-form', $this->theme);
     }
 }
