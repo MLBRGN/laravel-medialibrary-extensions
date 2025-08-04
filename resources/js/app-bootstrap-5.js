3a0b3c634ed3a10b5dcc5934f9d0f5fd10d75fc4
Evertjan Garretsen <evertjan@mlbrgn.nl>
2025-08-05 00:50:54 +0200

    dependency image editor fixes

--- resources/js/app-bootstrap-5.js
+++ resources/js/app-bootstrap-5.js
@@ -4,5 +4,5 @@ import '@/js/bootstrap-5/media-modal';
 import '@/js/bootstrap-5/image-editor-modal';
 import '@/css/app-bootstrap-5.scss';
 import '@/js/image-editor-listener'
-import ImageEditor from "@evertjanmlbrgn/ImageEditor/ImageEditor";// imported for side effects
+import ImageEditor from "@evertjanmlbrgn/imageeditor";// imported for side effects
 import bus from '@evertjanmlbrgn/imageshared/EventBus.js'// imported for side effects
--- resources/js/app-plain.js
+++ resources/js/app-plain.js
@@ -5,5 +5,6 @@ import '@/js/plain/media-modal';
 import '@/js/plain/image-editor-modal';
 import '@/css/app-plain.scss';
 import '@/js/image-editor-listener'
-import ImageEditor from "@evertjanmlbrgn/ImageEditor/ImageEditor";// imported for side effects
+// import ImageEditor from "@evertjanmlbrgn/ImageEditor/ImageEditor";// imported for side effects
+import "@evertjanmlbrgn/imageeditor";// imported for side effects
 import bus from '@evertjanmlbrgn/imageshared/EventBus.js'// imported for side effects
