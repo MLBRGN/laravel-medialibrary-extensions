978dc5c0ab6cb9b8b4d02ec92b48cb0c6570d0b2
Evertjan Garretsen <evertjan@mlbrgn.nl>
2025-09-02 19:56:52 +0200

    fixes to image inserting in tinymce custom file picker added image classes tiny, small, medium, large, full-width

--- resources/js/shared/tinymce-custom-file-picker.js
+++ resources/js/shared/tinymce-custom-file-picker.js
@@ -1,3 +1,4 @@
+// should be loaded when the custom file picker is enabled
 window.mleFilePicker = (callback, value, meta) => {
     if (meta.filetype === 'image') {
         const editor = tinymce.activeEditor;
@@ -33,7 +34,74 @@ window.mleFilePicker = (callback, value, meta) => {
                 console.warn('No file');
                 return;
             }
-            callback(file.url, { alt: file.alt || '' });
+            console.log('just before callback')
+            console.log('file', file);
+
+            // --- Your code needs to be here to handle the different file types ---
+            if (meta.filetype === 'image') {
+                console.log('image', file);
+                const img = new Image();
+                img.src = file.url;
+                img.onload = () => {
+                    const width = img.naturalWidth;
+                    const height = img.naturalHeight;
+                    const data = {
+                        alt: file.alt || '',
+                        classes: '',
+                        width: width + 'px',
+                        height: height + 'px',
+                        vspace: file.vspace || '0px',
+                        hspace: file.hspace || '0px',
+                        border: file.border || '0px',
+                        borderstyle: file.borderstyle || 'none',
+                    };
+                    console.log('data', data);
+                    callback(file.url, data);
+                };
+                img.onerror = () => {
+                    console.warn('Could not load image for dimensions');
+                    callback(file.url, {
+                        alt: file.alt || '',
+                    });
+                };
+            } else if (meta.filetype === 'media') {
+                // Logic for media files
+                callback(file.url, {
+                    source2: file.source2 || '',
+                    poster: file.poster || ''
+                });
+            } else if (meta.filetype === 'file') {
+                // Logic for general files
+                callback(file.url, {
+                    text: file.name || '',
+                    title: file.title || ''
+                });
+            }
+            // get dimensions
+            // const img = new Image();
+            // img.src = file.url;
+            // img.onload = () => {
+            //     const width = img.naturalWidth;
+            //     const height = img.naturalHeight;
+            //
+            //     console.log('width', width, 'height', height);
+            //     callback(file.url, {
+            //         alt: file.alt || '',
+            //         classes: 'mle-media-tinymce-image',
+            //         // width: file.width || '',
+            //         // height: file.height || '',
+            //         // vspace: file.vspace || '',
+            //         // hspace: file.hspace || '',
+            //         // border: file.border || '',
+            //         // borderstyle: file.borderstyle || ''
+            //     });
+            // };
+            // img.onerror = () => {
+            //     console.warn('Could not load image for dimensions');
+            //     callback(file.url, {
+            //         alt: file.alt || '',
+            //     });
+            // };
 
             window.removeEventListener('message', handleMessage);
             win.close();
--- resources/views/components/bootstrap-5/media-manager-tinymce.blade.php
+++ resources/views/components/bootstrap-5/media-manager-tinymce.blade.php
@@ -23,41 +23,4 @@
     include-js="true"
     :frontend-theme="$frontendTheme"
 />
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
-        document.getElementById('insert-selected').addEventListener('click', () => {
-            // TinyMCE injects a global `tinymce` object in the iframe window
-            // so you can do:
-            tinymce = window.parent.tinymce;
-            console.log('tinymce:', tinymce);
-
-            const selected = Array.from(document.querySelectorAll('.mle-media-select-checkbox:checked'))
-                .map(checkbox => ({ url: checkbox.dataset.url, alt: checkbox.dataset.alt || '' }));
-
-            if (!selected.length) {
-                tinymce.activeEditor.windowManager.alert('Please select one image.');
-                return;
-            }
-            if (selected.length > 1) {
-                tinymce.activeEditor.windowManager.alert('Please select only one image.');
-                return;
-            }
-
-            // Always pick only the first selected image
-            const file = selected[0];
-            console.log('Selected file:', file);
-
-            // Close the popup if needed
-            window.close();
-            console.log('Selected', selected);
-
-            // TinyMCE will catch this in onMessage
-            window.parent.postMessage({
-                mce: true,
-                content: file
-            }, '*'); // use '*' for now
-
-            console.log(window.parent.tinymce?.activeEditor?.windowManager?.params)
-        });
-    });
-</script>
+<script type="module" src="{{ asset('vendor/mlbrgn/media-library-extensions/tinymce-custom-file-picker-iframe.js') }}"></script>
