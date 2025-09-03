97e509363ca8faa17dbc549d5e21bebacdffbb46
Evertjan Garretsen <evertjan@mlbrgn.nl>
2025-09-03 22:04:33 +0200

    disabled favicon (for now) for media-manager-tinymce-wrapper.blade.php

--- resources/views/media-manager-tinymce-wrapper.blade.php
+++ resources/views/media-manager-tinymce-wrapper.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Component tests: theme bootstrap-5</title>
-        <link rel="icon" type="image/x-icon" href="{{ route('mle.favicon') }}">
+{{--        <link rel="icon" type="image/x-icon" href="{{ route('mle.favicon') }}">--}}
         {{--    <link--}}
         {{--        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"--}}
         {{--        rel="stylesheet"--}}
