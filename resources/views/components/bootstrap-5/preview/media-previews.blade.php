732d8b8bcf2a8215e06903956c49ff9b2500d7ea
Evertjan Garretsen <evertjan@mlbrgn.nl>
2025-11-06 22:08:19 +0100

    fixed previous problem, put aspect ratio on mle-media-preview-item instead of .mle-media-preview-item-container

--- resources/views/components/bootstrap-5/preview/media-previews.blade.php
+++ resources/views/components/bootstrap-5/preview/media-previews.blade.php
@@ -7,33 +7,29 @@
         ]) }}
         data-mle-media-preview-container
     >
-        {{-- TODO look at this, without the wrapper flex goes wrong 
-            with tall images --}}
-        <div class="extra-wrapper-for-flex">
-            <x-mle-media-preview-item
-                :id="$id"
-                :medium="$medium"
-                :options="$options"
-                :collections="$collections"
-                :single-medium="$singleMedium"
-                :model-or-class-name="$modelOrClassName"
-                :loop-index="$loop->index"
-                :selectable="$selectable"
-                :disabled="$disabled"
-                :readonly="$readonly"
-                :multiple="$multiple"
-            />
-            <x-mle-media-modal
-                :id="$id"
-                :model-or-class-name="$modelOrClassName"
-                :single-medium="$singleMedium"
-                :collections="$collections"
-                :single-medium="$singleMedium"
-                :video-auto-play="true"
-                :options="$options"
-                title="Media carousel"
-            />
-        </div>
+        <x-mle-media-preview-item
+            :id="$id"
+            :medium="$medium"
+            :options="$options"
+            :collections="$collections"
+            :single-medium="$singleMedium"
+            :model-or-class-name="$modelOrClassName"
+            :loop-index="$loop->index"
+            :selectable="$selectable"
+            :disabled="$disabled"
+            :readonly="$readonly"
+            :multiple="$multiple"
+        />
+        <x-mle-media-modal
+            :id="$id"
+            :model-or-class-name="$modelOrClassName"
+            :single-medium="$singleMedium"
+            :collections="$collections"
+            :single-medium="$singleMedium"
+            :video-auto-play="true"
+            :options="$options"
+            title="Media carousel"
+        />
         @if($getConfig('showMenu'))
             <x-mle-media-preview-menu
                 :id="$id"
