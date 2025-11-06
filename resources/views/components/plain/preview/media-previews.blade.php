544c5c99405464dbfc6dcc4734c350fa64b4565e
Evertjan Garretsen <evertjan@mlbrgn.nl>
2025-11-06 21:52:13 +0100

    temporary fix with todo added

--- resources/views/components/plain/preview/media-previews.blade.php
+++ resources/views/components/plain/preview/media-previews.blade.php
@@ -7,31 +7,33 @@
         ]) }}
         data-mle-media-preview-container
     >
-        <x-mle-media-preview-item
-            :id="$id"
-            :medium="$medium"
-            :options="$options"
-            :collections="$collections"
-            :single-medium="$singleMedium"
-            :model-or-class-name="$modelOrClassName"
-            :loop-index="$loop->index"
-            :selectable="$selectable"
-            :disabled="$disabled"
-            :readonly="$readonly"
-            :multiple="$multiple"
-        />
-
-        <x-mle-media-modal
-            :id="$id"
-            :model-or-class-name="$modelOrClassName"
-            :single-medium="$singleMedium"
-            :collections="$collections"
-            :single-medium="$singleMedium"
-            :video-auto-play="true"
-            :options="$options"
-            title="Media carousel"
-        />
-
+        {{-- TODO look at this, without the wrapper flex goes wrong 
+            with tall images --}}
+        <div class="extra-wrapper-for-flex">
+            <x-mle-media-preview-item
+                :id="$id"
+                :medium="$medium"
+                :options="$options"
+                :collections="$collections"
+                :single-medium="$singleMedium"
+                :model-or-class-name="$modelOrClassName"
+                :loop-index="$loop->index"
+                :selectable="$selectable"
+                :disabled="$disabled"
+                :readonly="$readonly"
+                :multiple="$multiple"
+            />
+            <x-mle-media-modal
+                :id="$id"
+                :model-or-class-name="$modelOrClassName"
+                :single-medium="$singleMedium"
+                :collections="$collections"
+                :single-medium="$singleMedium"
+                :video-auto-play="true"
+                :options="$options"
+                title="Media carousel"
+            />
+        </div>
         @if($getConfig('showMenu'))
             <x-mle-media-preview-menu
                 :id="$id"
